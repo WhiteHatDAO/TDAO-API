0a7ca89825c27e83aad5f245b5d0dafb3d99357d
Jaxcoder <codenamejason@outlook.com>
2022-04-07 14:55:08 -0400

    add node engine

--- index.js
+++ index.js
@@ -8,22 +8,25 @@ const articleRouter = require("./routes/article-router");
 const contactRouter = require("./routes/contact-router");
 
 const app = express();
-const apiPort = 4000;
+const apiPort = 4001;
 
-app.use(express.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 app.use(bodyParser.json());
 
-connection.on("error", console.error.bind(console, "MongoDB connection error:"));
+connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error: ")
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ name: "Talent DAO API", version: "v1.0" });
 });
 
 app.use("/api", authorRouter);
 app.use("/api", articleRouter);
-app.use("/api", contactRouter)
+app.use("/api", contactRouter);
 
 app.listen(apiPort, () => {
   console.log(`Server running on port ${apiPort}`);
